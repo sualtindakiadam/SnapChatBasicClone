3996f78bc22f156d8de642e398568ed40509d4bb
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-27 15:22:42 +0300

    App added

---
+++ SnapChatBasicClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapChatBasicClone
+//
+//  Created by Semih Kalaycı on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
